7659d068aa6d1c6cf043c21d1c12b4d09c52b504
maheswar-wayne <maheaswerd@gmail.com>
2025-01-28 10:33:57 +0530

    fix: size fix

--- src/pages/XRHome.tsx
+++ src/pages/XRHome.tsx
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 import { Canvas } from '@react-three/fiber';
 import { XR } from '@react-three/xr';
 import XrCube from '../components/XRContainer';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { createXRStore } from '@react-three/xr';
 
 const store = createXRStore();
@@ -10,37 +10,14 @@ const store = createXRStore();
 const XrContainer = () => {
     const { id } = useParams();
 
-    const [scale, setScale] = useState(5);
-
-    useEffect(() => {
-        const updateScale = () => {
-            if (window.innerWidth >= 1024) {
-                // Large screen
-                setScale(4);
-            } else {
-                // Mobile screen
-                setScale(0.5);
-            }
-        };
-
-        updateScale();
-        window.addEventListener('resize', updateScale);
-
-        return () => {
-            window.removeEventListener('resize', updateScale);
-        };
-    }, []);
+    const [scale] = useState(7);
 
     return (
         <>
             <div className="p-4 flex justify-center h-[50vh] md:w-[40vw] border-2 w-screen">
                 <Canvas resize={{ scroll: true, debounce: { scroll: 50, resize: 0 } }}>
                     <XR store={store}>
-                        <XrCube
-                            position={[0, -1, 0]}
-                            scale={scale}
-                            fileId={id ?? ''}
-                        />
+                        <XrCube position={[0, -1, 0]} scale={scale} fileId={id ?? ''} />
                     </XR>
                 </Canvas>
             </div>
