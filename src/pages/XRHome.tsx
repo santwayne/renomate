95fa4a5f927e66bec93e56c6ebf4d1254c21b383
sant kaur <kaursant79@gmail.com>
2025-02-12 16:02:00 +0530

    add heading

--- src/pages/XRHome.tsx
+++ src/pages/XRHome.tsx
@@ -9,21 +9,23 @@ const store = createXRStore();
 
 const XrContainer = () => {
     const { id } = useParams();
-
     const [scale] = useState(3);
 
     return (
         <>
-            <div className="p-4 flex justify-center h-[450px] w-full border-2">
-                <Canvas resize={{ scroll: true, debounce: { scroll: 50, resize: 0 } }}>
-                    <XR store={store}>
-                        <XrCube
-                            position={[0, -3, 0]}
-                            scale={scale}
-                            fileId={id ?? ''}
-                        />
-                    </XR>
-                </Canvas>
+            <div className="p-4 flex flex-col items-center h-[500px] w-full border-2">
+                <h2 className="text-lg font-semibold mb-2">3D View</h2>
+                <div className="flex justify-center h-[450px] w-full">
+                    <Canvas resize={{ scroll: true, debounce: { scroll: 50, resize: 0 } }}>
+                        <XR store={store}>
+                            <XrCube
+                                position={[0, -3, 0]}
+                                scale={scale}
+                                fileId={id ?? ''}
+                            />
+                        </XR>
+                    </Canvas>
+                </div>
             </div>
         </>
     );
